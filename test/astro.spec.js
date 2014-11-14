0f3841e1d00db7a7ce2addc25f31b533cd29f5e1
Keith Bingman <keith@keithbingman.com>
2014-11-14 10:12:22 -0800

    test setup

---
+++ test/astro.spec.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+var Astro = require('../src/astro');
+
+describe('Astro',function(){
+  // before(function(done){
+  //
+  // });
+
+  it('should be defined',function(){
+    expect(Astro).to.be.defined;
+  });
+
+});
---
+++ test/dust_bands.spec.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+var DustBands = require('../src/dust_bands');
+
+describe('DustBands',function(){
+  // before(function(done){
+  //
+  // });
+
+  it('should be defined',function(){
+    expect(DustBands).to.be.defined;
+  });
+
+});
